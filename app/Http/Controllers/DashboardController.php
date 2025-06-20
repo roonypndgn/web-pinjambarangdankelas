911e16542c4a0984fec79ae1ed194bb84614e48f
AfriDomenicoTrg <herocorwn181@gmail.com>
2025-06-20 18:22:56 +0700

    Menyelesaikan Peminjaman dan Dashboard Controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+       if (Auth::user()->jenis == 'admin') {
+        // Tampilkan dashboard admin
+        return view('dashboard.admin');
+    } else {
+        // Redirect ke dashboard member
+        return redirect()->route('member.dashboard');
+    }
+}
+}
+
